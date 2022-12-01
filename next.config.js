648e0116e56473cbbbc02b4a892c459d2569aa04
refdd <titorefat76@example.com>
2022-12-01 16:46:05 +0200

    try again

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  optimizeFonts:true,
   images: {
     domains: ['api.nilecruisez.com'],
   },
