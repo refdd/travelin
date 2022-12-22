d8d16e9dd6914766c7c7a4495214c212439ae261
refdd <titorefat76@example.com>
2022-12-22 03:07:43 +0200

    refat night

--- next.config.js
+++ next.config.js
@@ -1,41 +1,7 @@
 const path = require("path");
-// const withVideos = require('next-videos');
-
 module.exports = {
 
   reactStrictMode: true,
 
-  // images: {
-  //   loader: "imgix",
-  // },
-
-  // rules: [
-  //   {
-  //     test: /\.less$/,
-  //     use: [
-  //       {
-  //         loader: "style-loader",
-  //       },
-  //       {
-  //         loader: "css-loader", // translates CSS into CommonJS
-  //       },
-  //       {
-  //         loader: "less-loader", // compiles Less to CSS
-  //         options: {
-  //           lessOptions: {
-  //             // If you are using less-loader@5 please spread the lessOptions to options directly
-             
-  //             javascriptEnabled: true,
-  //           },
-  //         },
-  //       },
-  //     ],
-  //     // ...other rules
-  //   },
-  // ],
-
-  // };
+  
 };
-
-
-// module.exports = withVideos()
